780f46ec3c61a047c4218eadc136a7ed1bc46c9f
Peter Hanley <hanley.peter@gmail.com>
2020-08-14 11:01:20 +1000

    Added more comments and refactoring

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ class Calculator {
     // add an event listener to the calculator element
     this.keys.addEventListener("click", (e) => {
       if (e.target.matches("button")) {
-        //calculator button clicked. Perform action
+        // a calculator button was clicked. Perform action
         this.buttonClicked(e.target);
       }
     });
@@ -17,7 +17,7 @@ class Calculator {
 
   clearInput(key) {
     if (key.textContent === "AC") {
-      // reset all stored values
+      // "AC" was clicked, clear all stored values
       this.calculator.dataset.firstValue = "";
       this.calculator.dataset.modValue = "";
       this.calculator.dataset.operator = "";
@@ -26,7 +26,7 @@ class Calculator {
       // change "CE" (clear current) to "AC" (clear all stored)
       key.textContent = "AC";
     }
-
+    // clear the current display ("CE")
     this.display.textContent = 0;
     this.calculator.dataset.previousKeyType = "clear";
   }
@@ -35,12 +35,7 @@ class Calculator {
     // get the button clicked
     const key = target;
 
-    // remove all "depressed" class from button elements
-    Array.from(key.parentNode.children).forEach((k) =>
-      k.classList.remove("is-depressed")
-    );
-
-    // get the data-action value of the button (if there is one)
+    // get the data-action value of the button (if there is one: clear / divide / multiply / subtract / add / calculate)
     const action = key.dataset.action;
 
     // get the value of the button clicked
@@ -52,17 +47,17 @@ class Calculator {
     // get the type of the previous button clicked
     const previousKeyType = this.calculator.dataset.previousKeyType;
 
-    // if button clicked is a numeral 0 - 9
+    // perform action if button clicked is a numeral 0 - 9
     if (!action) {
       this.numberInput(keyContent, displayedNum, previousKeyType);
     }
 
-    // if button clicked is a decimal point
+    // perform action if button clicked is a decimal point
     if (action === "decimal") {
       this.decimalInput(displayedNum, previousKeyType);
     }
 
-    // if button clicked is an operator (plus / minus / multiply / divide)
+    // perform action if button clicked is an operator (plus / minus / multiply / divide)
     if (
       action === "add" ||
       action === "subtract" ||
@@ -70,43 +65,43 @@ class Calculator {
       action === "divide"
     ) {
       this.operatorInput(displayedNum, previousKeyType, action);
-
-      key.classList.add("is-depressed");
     }
 
-    // if button clicked is clear
+    // perform action if button clicked is clear
     if (action === "clear") {
       this.clearInput(key);
     }
 
-    // if button clicked is not clear, change "AC" to "CE"
+    // if button clicked was not clear, change "AC" to "CE" ("clear all to clear current")
     if (action !== "clear") {
       const clearButton = this.calculator.querySelector("[data-action=clear]");
       clearButton.textContent = "CE";
     }
 
-    // if button clicked is equals, perform calculation
+    // perform calculation if button clicked is equals
     if (action === "calculate") {
       this.equalsInput(displayedNum, previousKeyType);
     }
   }
 
+  // function that deals with the click of a number button
   numberInput(keyContent, displayedNum, previousKeyType) {
     if (
       displayedNum === "0" ||
       previousKeyType === "operator" ||
       previousKeyType === "calculate"
     ) {
-      // display button value directly if it is the first number (i.e. display is zero)
+      // display button value directly on screen if it is the first number (i.e. display is zero)
       // or the previous button clicked was an operator or equals
       this.display.textContent = keyContent;
     } else {
-      // concatenate the button value onto the displayed number string
+      // concatenate the button value onto the displayed number string if previous button clicked was also a number
       this.display.textContent = displayedNum + keyContent;
     }
     this.calculator.dataset.previousKeyType = "number";
   }
 
+  // function that deals with the click of the decimal button
   decimalInput(displayedNum, previousKeyType) {
     if (!displayedNum.includes(".")) {
       // only add a decimal point to the display string if it hasn't been added already
@@ -115,12 +110,13 @@ class Calculator {
       previousKeyType === "operator" ||
       previousKeyType === "calculate"
     ) {
-      // display "0." if the previous button clicked wasn't a number
+      // display "0." if the previous button clicked wasn't a number (e.g. operator button)
       this.display.textContent = "0.";
     }
     this.calculator.dataset.previousKeyType = "decimal";
   }
 
+  // function that deals with the click of an operator button (+/-/x/÷).
   operatorInput(displayedNum, previousKeyType, action) {
     const firstValue = this.calculator.dataset.firstValue;
     const operator = this.calculator.dataset.operator;
@@ -132,45 +128,50 @@ class Calculator {
       previousKeyType !== "operator" &&
       previousKeyType !== "calculate"
     ) {
-      // perform a calculation if a previous value and operator exist
+      // perform a calculation if a previous value ("value one") and operator have already been pressed & stored,
+      // and if the previous button clicked ("value two") wasn't already an operator or equals button
       const calcValue = this.calculate(firstValue, operator, secondValue);
 
       // display the calculation result
       this.display.textContent = calcValue;
 
-      // set the previous value to the calculation result
+      // set "value one" to the calculation result
       this.calculator.dataset.firstValue = calcValue;
     } else {
+      // first value ("value one") does not exist yet. Save the displayed value into "value one" holder.
       this.calculator.dataset.firstValue = displayedNum;
     }
 
     this.calculator.dataset.previousKeyType = "operator";
+    // store the operator type into the "operator" holder
     this.calculator.dataset.operator = action;
   }
 
+  // function that deals with the click of the equals (=) button
   equalsInput(displayedNum, previousKeyType) {
     let firstValue = this.calculator.dataset.firstValue;
     const operator = this.calculator.dataset.operator;
     let secondValue = displayedNum;
 
+    // check to see if first value ("value one") exists.
+    // get first value from the first value store and second value from the screen
+    // get operator type from the operator store
+
     if (firstValue) {
-      if (previousKeyType === "calculate") {
-        firstValue = displayedNum;
-        secondValue = this.calculator.dataset.modValue;
+      // only perform a calculation if previous button pressed wasn't already an equals button
+
+      if (previousKeyType !== "calculate") {
+        this.display.textContent = this.calculate(
+          firstValue,
+          operator,
+          secondValue
+        );
       }
-
-      this.display.textContent = this.calculate(
-        firstValue,
-        operator,
-        secondValue
-      );
     }
-
-    // Set modValue attribute
-    this.calculator.dataset.modValue = secondValue;
     this.calculator.dataset.previousKeyType = "calculate";
   }
 
+  // function to perform calculation, takes two values & operator as input and returns the calculated value
   calculate(n1, operator, n2) {
     const firstNum = parseFloat(n1);
     const secondNum = parseFloat(n2);
