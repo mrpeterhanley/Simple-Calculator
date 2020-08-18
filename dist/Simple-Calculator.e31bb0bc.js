d3150ddb80d4d0c89265e3c1bc142ffa44a94d4e
Peter Hanley <hanley.peter@gmail.com>
2020-08-18 13:35:50 +1000

    Changed opacity setting

--- dist/Simple-Calculator.e31bb0bc.js
+++ dist/Simple-Calculator.e31bb0bc.js
@@ -371,7 +371,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "5968" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "6310" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
